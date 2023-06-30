6a03eb6a678f0292cd5e4916b4f96e67bf23d889
Marco Tiraboschi <marcotiraboschi@hotmail.it>
2023-06-30 13:49:21 +0200

    :white_check_mark: Test OSC getter methods

--- tests/TestSDTOSCAnalysis.c
+++ tests/TestSDTOSCAnalysis.c
@@ -110,4 +110,53 @@ void TestSDTOSCZeroCrossing_loads(CuTest *tc) {
   SDTOSC_ZEROX_TEST_END()
 }
 
+void TestSDTOSCZeroCrossing_setOverlap(CuTest *tc) {
+  SDTOSC_ZEROX_TEST_BEGIN("/zerox/overlap", 2)
+  CuAssert(tc, "Fail on too few args", SDTOSCRoot(short_msg) != 0);
+  CuAssert(tc, "Fail on uninitialized args", SDTOSCRoot(msg) != 0);
+
+  SDTOSCArgumentList_setArgument(args, 0, SDTOSCArgument_newString(key));
+  CuAssert(tc, "Fail on object not found", SDTOSCRoot(msg) != 0);
+
+  SDT_registerZeroCrossing(zx, key);
+  CuAssert(tc, "Fail on uninitialized overlap", SDTOSCRoot(msg) != 0);
+
+  SDTRandomSequence *olaps = SDTRandomSequence_newFloat(16, 0, 0.999);
+  SDTOSCArgument *a;
+  FOR_RANDOM_ITER_FLOAT (olaps, f) {
+    a = SDTOSCArgumentList_setArgument(args, 1, SDTOSCArgument_newFloat(f));
+    if (a) SDTOSCArgument_free(a);
+    SDTOSCRoot(msg);
+    CuAssertDblEquals(tc, f, SDTZeroCrossing_getOverlap(zx), 0.005);
+  }
+  SDTRandomSequence_free(olaps);
+  SDT_unregisterZeroCrossing(key);
+  SDTOSC_ZEROX_TEST_END()
+}
+
+void TestSDTOSCZeroCrossing_setSize(CuTest *tc) {
+  SDTOSC_ZEROX_TEST_BEGIN("/zerox/size", 2)
+  CuAssert(tc, "Fail on too few args", SDTOSCRoot(short_msg) != 0);
+  CuAssert(tc, "Fail on uninitialized args", SDTOSCRoot(msg) != 0);
+
+  SDTOSCArgumentList_setArgument(args, 0, SDTOSCArgument_newString(key));
+  CuAssert(tc, "Fail on object not size", SDTOSCRoot(msg) != 0);
+
+  SDT_registerZeroCrossing(zx, key);
+  CuAssert(tc, "Fail on uninitialized overlap", SDTOSCRoot(msg) != 0);
+
+  SDTRandomSequence *sizes = SDTRandomSequence_newLog(16, 1 << 8, 1 << 15);
+  SDTOSCArgument *a;
+  FOR_RANDOM_ITER_FLOAT (sizes, s) {
+    a = SDTOSCArgumentList_setArgument(args, 1,
+                                       SDTOSCArgument_newFloat((int)s));
+    if (a) SDTOSCArgument_free(a);
+    SDTOSCRoot(msg);
+    CuAssertDblEquals(tc, (int)s, SDTZeroCrossing_getSize(zx), 0.005);
+  }
+  SDTRandomSequence_free(sizes);
+  SDT_unregisterZeroCrossing(key);
+  SDTOSC_ZEROX_TEST_END()
+}
+
 // ----------------------------------------------------------------------------
