aaacadb6b74c9a4258d95d3e7a148812a485134b
Marco Tiraboschi <marcotiraboschi@hotmail.it>
2023-10-03 15:38:37 +0200

    :sparkles: Implement OSC methods for Breaking

--- src/SDT/OSC/SDTOSCControl.c
+++ src/SDT/OSC/SDTOSCControl.c
@@ -41,3 +41,43 @@ _SDTOSC_FLOAT_SETTER_FUNCTION(Bouncing, restitution, Restitution, double)
 _SDTOSC_FLOAT_SETTER_FUNCTION(Bouncing, height, Height, double)
 _SDTOSC_FLOAT_SETTER_FUNCTION(Bouncing, irregularity, Irregularity, double)
 /* ------------------------------------------------------------------------- */
+
+/* --- Breaking -------------------------------------------------------- */
+int SDTOSCBreaking(const SDTOSCMessage* x) {
+  SDTOSC_MESSAGE_LOGA(VERBOSE, "\n  %s\n", x, "");
+  const SDTOSCAddress* a = SDTOSCMessage_getAddress(x);
+  if (SDTOSCAddress_getDepth(a) < 2) {
+    SDTOSC_MESSAGE_LOGA(ERROR,
+                        "\n  %s\n  [MISSING METHOD] Please, specify an OSC "
+                        "method from the container\n  %s\n",
+                        x, SDTOSC_rtfm_string());
+    return 1;
+  }
+  const char* k = SDTOSCAddress_getNode(a, 1);
+  if (!strcmp("log", k)) return SDTOSCBreaking_log(x);
+  if (!strcmp("save", k)) return SDTOSCBreaking_save(x);
+  if (!strcmp("load", k)) return SDTOSCBreaking_load(x);
+  if (!strcmp("loads", k)) return SDTOSCBreaking_loads(x);
+  if (!strcmp("storedEnergy", k) || !strcmp("stored", k))
+    return SDTOSCBreaking_setStoredEnergy(x);
+  if (!strcmp("crushingEnergy", k) || !strcmp("crushing", k))
+    return SDTOSCBreaking_setCrushingEnergy(x);
+  if (!strcmp("granularity", k)) return SDTOSCBreaking_setGranularity(x);
+  if (!strcmp("fragmentation", k)) return SDTOSCBreaking_setFragmentation(x);
+  SDTOSC_MESSAGE_LOGA(ERROR,
+                      "\n  %s\n  [NOT IMPLEMENTED] The specified method is not "
+                      "implemented: %s\n  %s\n",
+                      x, k, SDTOSC_rtfm_string());
+  return 2;
+}
+
+_SDTOSC_LOG_FUNCTION(Breaking)
+_SDTOSC_SAVE_FUNCTION(Breaking)
+_SDTOSC_LOAD_FUNCTION(Breaking)
+_SDTOSC_LOADS_FUNCTION(Breaking)
+
+_SDTOSC_FLOAT_SETTER_FUNCTION(Breaking, storedEnergy, StoredEnergy, double)
+_SDTOSC_FLOAT_SETTER_FUNCTION(Breaking, crushingEnergy, CrushingEnergy, double)
+_SDTOSC_FLOAT_SETTER_FUNCTION(Breaking, granularity, Granularity, double)
+_SDTOSC_FLOAT_SETTER_FUNCTION(Breaking, fragmentation, Fragmentation, double)
+/* ------------------------------------------------------------------------- */
