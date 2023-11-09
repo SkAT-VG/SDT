4eebc70970e24ffbf8d04029777ee4876fbf4aca
Marco Tiraboschi <marcotiraboschi@hotmail.it>
2023-11-09 16:40:20 +0100

    :sparkles: Implement OSC methods for FluidFlow

--- src/SDT/OSC/SDTOSCLiquids.c
+++ src/SDT/OSC/SDTOSCLiquids.c
@@ -6,7 +6,7 @@
 
 #include "../SDTCommon.h"
 
-/* --- Bubble -------------------------------------------------------- */
+/* --- Bubble -------------------------------------------------------------- */
 int SDTOSCBubble(const SDTOSCMessage* x) {
   SDTOSC_MESSAGE_LOGA(VERBOSE, "\n  %s\n", x, "");
   const SDTOSCAddress* a = SDTOSCMessage_getAddress(x);
@@ -51,4 +51,53 @@ int SDTOSCBubble_trigger(const SDTOSCMessage* x) {
   SDTBubble_normAmp(obj);
   return 0;
 }
+/* ------------------------------------------------------------------------- */
+
+/* --- FluidFlow ----------------------------------------------------------- */
+int SDTOSCFluidFlow(const SDTOSCMessage* x) {
+  SDTOSC_MESSAGE_LOGA(VERBOSE, "\n  %s\n", x, "");
+  const SDTOSCAddress* a = SDTOSCMessage_getAddress(x);
+  if (SDTOSCAddress_getDepth(a) < 2) {
+    SDTOSC_MESSAGE_LOGA(ERROR,
+                        "\n  %s\n  [MISSING METHOD] Please, specify an OSC "
+                        "method from the container\n  %s\n",
+                        x, SDTOSC_rtfm_string());
+    return 1;
+  }
+  const char* k = SDTOSCAddress_getNode(a, 1);
+  if (!strcmp("log", k)) return SDTOSCFluidFlow_log(x);
+  if (!strcmp("save", k)) return SDTOSCFluidFlow_save(x);
+  if (!strcmp("load", k)) return SDTOSCFluidFlow_load(x);
+  if (!strcmp("loads", k)) return SDTOSCFluidFlow_loads(x);
+  if (!strcmp("avgRate", k) || !strcmp("rate", k))
+    return SDTOSCFluidFlow_setAvgRate(x);
+  if (!strcmp("minRadius", k)) return SDTOSCFluidFlow_setMinRadius(x);
+  if (!strcmp("maxRadius", k)) return SDTOSCFluidFlow_setMaxRadius(x);
+  if (!strcmp("expRadius", k)) return SDTOSCFluidFlow_setExpRadius(x);
+  if (!strcmp("minDepth", k)) return SDTOSCFluidFlow_setMinDepth(x);
+  if (!strcmp("maxDepth", k)) return SDTOSCFluidFlow_setMaxDepth(x);
+  if (!strcmp("expDepth", k)) return SDTOSCFluidFlow_setExpDepth(x);
+  if (!strcmp("riseFactor", k)) return SDTOSCFluidFlow_setRiseFactor(x);
+  if (!strcmp("riseCutoff", k)) return SDTOSCFluidFlow_setRiseCutoff(x);
+  SDTOSC_MESSAGE_LOGA(ERROR,
+                      "\n  %s\n  [NOT IMPLEMENTED] The specified method is not "
+                      "implemented: %s\n  %s\n",
+                      x, k, SDTOSC_rtfm_string());
+  return 2;
+}
+
+_SDTOSC_LOG_FUNCTION(FluidFlow)
+_SDTOSC_SAVE_FUNCTION(FluidFlow)
+_SDTOSC_LOAD_FUNCTION(FluidFlow)
+_SDTOSC_LOADS_FUNCTION(FluidFlow)
+
+_SDTOSC_FLOAT_SETTER_FUNCTION(FluidFlow, avgRate, AvgRate, double)
+_SDTOSC_FLOAT_SETTER_FUNCTION(FluidFlow, minRadius, MinRadius, double)
+_SDTOSC_FLOAT_SETTER_FUNCTION(FluidFlow, maxRadius, MaxRadius, double)
+_SDTOSC_FLOAT_SETTER_FUNCTION(FluidFlow, expRadius, ExpRadius, double)
+_SDTOSC_FLOAT_SETTER_FUNCTION(FluidFlow, minDepth, MinDepth, double)
+_SDTOSC_FLOAT_SETTER_FUNCTION(FluidFlow, maxDepth, MaxDepth, double)
+_SDTOSC_FLOAT_SETTER_FUNCTION(FluidFlow, expDepth, ExpDepth, double)
+_SDTOSC_FLOAT_SETTER_FUNCTION(FluidFlow, riseFactor, RiseFactor, double)
+_SDTOSC_FLOAT_SETTER_FUNCTION(FluidFlow, riseCutoff, RiseCutoff, double)
 /* ------------------------------------------------------------------------- */
