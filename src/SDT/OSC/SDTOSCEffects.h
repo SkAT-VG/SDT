13cfaf15e5b2f455712f25ae302058083067c312
Marco Tiraboschi <marcotiraboschi@hotmail.it>
2023-11-27 15:51:08 +0100

    :sparkles: Implement OSC methods for Reverb

--- src/SDT/OSC/SDTOSCEffects.h
+++ src/SDT/OSC/SDTOSCEffects.h
@@ -83,6 +83,89 @@ extern int SDTOSCPitchShift(const SDTOSCMessage *x);
 
 /** @} */
 
+/** @defgroup oscreverb SDTOSCReverb
+OSC for #SDTReverb objects
+@ingroup oscmethods
+@{ */
+
+/** @brief `/reverb/log <name>`
+
+Function that implements OSC JSON log for #SDTReverb objects
+@param x OSC message
+@return Zero on success, non-zero otherwise */
+extern int SDTOSCReverb_log(const SDTOSCMessage *x);
+
+/** @brief `/reverb/save <name> <filepath>`
+
+Function that implements OSC JSON save for #SDTReverb objects
+@param x OSC message
+@return Zero on success, non-zero otherwise */
+extern int SDTOSCReverb_save(const SDTOSCMessage *x);
+
+/** @brief `/reverb/load <name> <filepath>`
+
+Function that implements OSC JSON file loading for #SDTReverb objects
+@param x OSC message
+@return Zero on success, non-zero otherwise */
+extern int SDTOSCReverb_load(const SDTOSCMessage *x);
+
+/** @brief `/reverb/loads <name> <json_string>`
+
+Function that implements OSC JSON loading from string for #SDTReverb
+objects
+@param x OSC message
+@return Zero on success, non-zero otherwise */
+extern int SDTOSCReverb_loads(const SDTOSCMessage *x);
+
+/** @brief `/reverb/xSize <name> <value>`
+
+Function that implements OSC parameter setting for #SDTReverb objects
+@param x OSC message
+@return Zero on success, non-zero otherwise */
+extern int SDTOSCReverb_setXSize(const SDTOSCMessage *x);
+
+/** @brief `/reverb/ySize <name> <value>`
+
+Function that implements OSC parameter setting for #SDTReverb objects
+@param x OSC message
+@return Zero on success, non-zero otherwise */
+extern int SDTOSCReverb_setYSize(const SDTOSCMessage *x);
+
+/** @brief `/reverb/zSize <name> <value>`
+
+Function that implements OSC parameter setting for #SDTReverb objects
+@param x OSC message
+@return Zero on success, non-zero otherwise */
+extern int SDTOSCReverb_setZSize(const SDTOSCMessage *x);
+
+/** @brief `/reverb/randomness <name> <value>`
+
+Function that implements OSC parameter setting for #SDTReverb objects
+@param x OSC message
+@return Zero on success, non-zero otherwise */
+extern int SDTOSCReverb_setRandomness(const SDTOSCMessage *x);
+
+/** @brief `/reverb/time <name> <value>`
+
+Function that implements OSC parameter setting for #SDTReverb objects
+@param x OSC message
+@return Zero on success, non-zero otherwise */
+extern int SDTOSCReverb_setTime(const SDTOSCMessage *x);
+
+/** @brief `/reverb/time1k <name> <value>`
+
+Function that implements OSC parameter setting for #SDTReverb objects
+@param x OSC message
+@return Zero on success, non-zero otherwise */
+extern int SDTOSCReverb_setTime1k(const SDTOSCMessage *x);
+
+/** @brief `/reverb/...`
+
+Function that routes OSC commands for #SDTReverb objects
+@param x OSC message pointer
+@return Zero on success, non-zero otherwise */
+extern int SDTOSCReverb(const SDTOSCMessage *x);
+
 /** @} */
 
 #ifdef __cplusplus
