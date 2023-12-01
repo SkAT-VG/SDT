d218defa50ba5411eec67af477968243318a542e
Marco Tiraboschi <marcotiraboschi@hotmail.it>
2023-12-01 15:34:06 +0100

    :fire: Remove runaway redundant declarations for demix hashmap functions

--- src/SDT/SDTDemix.h
+++ src/SDT/SDTDemix.h
@@ -75,6 +75,12 @@ Otherwise, a NULL pointer is returned.
 @return Demix process instance pointer */
 extern SDTDemix *SDT_getDemix(const char *key);
 
+/** @brief Unregisters a demixer from the demixers list. If a demixer with the
+given ID is present, it is unregistered from the list.
+@param[in] key Unique ID of the demixer instance to unregister
+@return Zero on success, otherwise one */
+extern int SDT_unregisterDemix(const char *key);
+
 /** @brief Gets the analysis window length.
 @param[in] x Pointer to the instance
 @return Analysis window length, in samples */
@@ -99,12 +105,6 @@ extern double SDTDemix_getNoiseThreshold(const SDTDemix *x);
 @param[in] x Pointer to the instance
 @return Amount of non-residual falling into the tonal category */
 extern double SDTDemix_getTonalThreshold(const SDTDemix *x);
-
-/** @brief Unregisters a demixer from the demixers list. If a demixer with the
-given ID is present, it is unregistered from the list.
-@param[in] key Unique ID of the demixer instance to unregister
-@return Zero on success, otherwise one */
-extern int SDT_unregisterDemix(const char *key);
 
 /** @brief Represent a demixer as a JSON object.
 @param[in] x Pointer to the instance
@@ -151,23 +151,6 @@ percussive/harmonic/residual components
 component, outs[1] is the harmonic component and outs[2] is the residual.
 @param[in] in Input sample */
 extern void SDTDemix_dsp(SDTDemix *x, double *outs, double in);
-
-/** @brief Registers a demixer into the demixers list with a unique ID.
-@param[in] x Demix instance to register
-@param[in] key Unique ID assigned to the demixer instance */
-extern int SDT_registerDemix(SDTDemix *x, const char *key);
-
-/** @brief Queries the demixers list by its unique ID.
-If a demixer with the ID is present, a pointer to the demixer is returned.
-Otherwise, a NULL pointer is returned.
-@param[in] key Unique ID assigned to the Demix instance
-@return Demix instance pointer */
-extern SDTDemix *SDT_getDemix(const char *key);
-
-/** @brief Unregisters a demixer from the demixers list.
-If a demixer with the given ID is present, it is unregistered from the list.
-@param[in] key Unique ID of the Demix instance to unregister */
-extern int SDT_unregisterDemix(const char *key);
 
 #ifdef __cplusplus
 };
