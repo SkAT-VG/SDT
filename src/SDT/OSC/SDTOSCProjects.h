1f266c9fe439db9f0035a2526e911f9166a05144
Marco Tiraboschi <marcotiraboschi@hotmail.it>
2023-12-06 11:47:14 +0100

    :sparkles: Write OSC projects header

--- src/SDT/OSC/SDTOSCProjects.h
+++ src/SDT/OSC/SDTOSCProjects.h
@@ -1,4 +1,3 @@
-#include "../SDTJSON.h"
 #include "SDTOSCCommon.h"
 
 /** @file SDTOSCProjects.h
@@ -14,109 +13,46 @@ OSC containers and methods for SDT Projects
 extern "C" {
 #endif
 
-/** @brief The container of OSC methods for SDT Projects
-\par OSC Address
-/project
-@param [in] log Log function pointer
-@param[in] x OSC message
-@return Return code */
-// extern SDTOSCReturnCode SDTOSCProject(void (* log)(const char *, ...), const
-// SDTOSCMessage* x);
-
-/** @brief OSC method for logging information about SDT Projects
-\par OSC Address
-/project/log
-\par OSC Arguments
-ID0 ID1 ...
-@param [in] log Log function pointer
-@param [in] args Additional OSC arguments: object IDs (strings)
-@return Return code */
-// extern SDTOSCReturnCode SDTOSCProject_log(void (* log)(const char *, ...),
-// const SDTOSCArgumentList* args);
-
-/** @brief OSC method for saving information about SDT Projects
-\par OSC Address
-/project/save
-\par OSC Arguments
-FILE ID0 ID1 ...
-@param [in] log Log function pointer
-@param [in] args Additional OSC arguments: file path (string), object IDs
-(strings)
-@return Return code */
-// extern SDTOSCReturnCode SDTOSCProject_save(void (* log)(const char *, ...),
-// const SDTOSCArgumentList* args);
-
-/** @brief OSC method for loading information for SDT Projects from a JSON file
-\par OSC Address
-/project/load
-\par OSC Arguments
-FILE
-@param [in] log Log function pointer
-@param [in] args Additional OSC arguments: file path (string)
-@return Return code */
-// SDTOSCReturnCode SDTOSCProject_load(void (*log)(const char*, ...), const
-// SDTOSCArgumentList* args);
-
-/** @defgroup osc_project_metadata Project Metadata
-OSC containers and methods for SDT Project Metadata
+/** @defgroup oscprojects SDTOSCProjects
+OSC for SDT projects
+@ingroup oscmethods
 @{
 */
 
-/** @brief The container of OSC methods for SDT Project Metadata
-\par OSC Address
-/project/metadata
-@param [in] log Log function pointer
-@param[in] x OSC message
-@return Return code */
-// extern SDTOSCReturnCode SDTOSCProjectMetadata(void (* log)(const char *,
-// ...), const SDTOSCMessage* x);
+/** @brief `/project/log <name_1> <name_2> ...`
 
-/** @brief OSC method for logging SDT Project Metadata
-\par OSC Address
-/project/metadata/log
-@param [in] log Log function pointer
-@return Return code */
-// extern SDTOSCReturnCode SDTOSCProjectMetadata_log(void (* log)(const char *,
-// ...));
+Function that implements OSC JSON log for SDT projects
+@param x OSC message
+@return Zero on success, non-zero otherwise */
+extern int SDTOSCProject_log(const SDTOSCMessage *x);
 
-/** @brief OSC method for saving SDT Project Metadata
-\par OSC Address
-/project/metadata/save
-@param [in] log Log function pointer
-@param [in] args Additional OSC arguments: file path (string)
-@return Return code */
-// extern SDTOSCReturnCode SDTOSCProjectMetadata_save(void (* log)(const char *,
-// ...), const SDTOSCArgumentList* args);
+/** @brief `/project/save <filepath> <name_1> <name_2> ...`
 
-/** @brief OSC method for loading SDT Project Metadata from a JSON file
-\par OSC Address
-/project/metadata/load
-\par OSC Arguments
-FILE
-@param [in] log Log function pointer
-@param [in] args Additional OSC arguments: file path (string)
-@return Return code */
-// extern SDTOSCReturnCode SDTOSCProjectMetadata_load(void (* log)(const char *,
-// ...), const SDTOSCArgumentList* args);
+Function that implements OSC JSON save for SDT projects
+@param x OSC message
+@return Zero on success, non-zero otherwise */
+extern int SDTOSCProject_save(const SDTOSCMessage *x);
 
-/** @brief OSC method for setting SDT Project Metadata
-\par OSC Address
-/project/metadata/set
-\par OSC Arguments
-KEY VALUE
-@param [in] log Log function pointer
-@param [in] args Additional OSC arguments: key (string), value (any)
-@return Return code */
-// extern SDTOSCReturnCode SDTOSCProjectMetadata_set(void (* log)(const char *,
-// ...), const SDTOSCArgumentList* args);
+/** @brief `/project/load <filepath>`
 
-/** @brief OSC method for resetting SDT Project Metadata to an empty object
-\par OSC Address
-/project/metadata/reset
-@param [in] log Log function pointer
-@return Return code */
-// extern SDTOSCReturnCode SDTOSCProjectMetadata_reset(void (* log)(const char
-// *, ...));
+Function that implements OSC JSON file loading for SDT projects
+@param x OSC message
+@return Zero on success, non-zero otherwise */
+extern int SDTOSCProject_load(const SDTOSCMessage *x);
+
+/** @brief `/project/loads <json_string>`
+
+Function that implements OSC JSON loading from string for SDT projects
+@param x OSC message
+@return Zero on success, non-zero otherwise */
+extern int SDTOSCProject_loads(const SDTOSCMessage *x);
+
+/** @brief `/project/...`
+
+Function that routes OSC commands for SDT projects
+@param x OSC message pointer
+@return Zero on success, non-zero otherwise */
+extern int SDTOSCProject(const SDTOSCMessage *x);
 
 /** @} */
 
